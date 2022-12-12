e02c8b4172f40c0be8ef1ca54560df9e60fdbc25
maengmaengmaeng <dta4718@naver.com>
2022-12-12 12:49:33 +0900

    미니프로잭트만드는중

--- MiniProject/src/com/demo/config/ServletAppContext.java
+++ MiniProject/src/com/demo/config/ServletAppContext.java
@@ -24,33 +24,33 @@ import com.demo.mapper.MapperInterface;
 @ComponentScan("com.demo.controller")
 @PropertySource("/WEB-INF/properties/db.properties")
 public class ServletAppContext implements WebMvcConfigurer {
-
+	
 	@Value("${db.classname}")
 	private String db_classname;
-
+	
 	@Value("${db.url}")
 	private String db_url;
-
+	
 	@Value("${db.username}")
 	private String db_username;
-
+	
 	@Value("${db.password}")
 	private String db_password;
-
+	
 	// Controller의 메서드가 반환하는 jsp의 이름 앞뒤에 경로와 확장자를 붙혀주도록 설정한다.
 	@Override
-	public void configureViewResolvers(ViewResolverRegistry registry) {
+	public void configureViewResolvers(ViewResolverRegistry registry) {		
 		WebMvcConfigurer.super.configureViewResolvers(registry);
 		registry.jsp("/WEB-INF/views/", ".jsp");
 	}
-
+	
 	// 정적 파일의 경로를 매핑한다.
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 		registry.addResourceHandler("/**").addResourceLocations("/resources/");
-	}
-
+	}	
+	
 	// 데이터베이스 접속 정보 관리
 	@Bean
 	public BasicDataSource dataSource() {
@@ -59,22 +59,22 @@ public class ServletAppContext implements WebMvcConfigurer {
 		source.setUrl(db_url);
 		source.setUsername(db_username);
 		source.setPassword(db_password);
-
+		
 		return source;
 	}
-
+	
 	// 쿼리문과 접속 관리하는 객체
 	@Bean
-	public SqlSessionFactory factory(BasicDataSource source) throws Exception {
+	public SqlSessionFactory factory(BasicDataSource source) throws Exception{
 		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
 		factoryBean.setDataSource(source);
 		SqlSessionFactory factory = factoryBean.getObject();
 		return factory;
 	}
-
+	
 	// 쿼리문 실행을 위한 객체
 	@Bean
-	public MapperFactoryBean<MapperInterface> test_mapper(SqlSessionFactory factory) throws Exception {
+	public MapperFactoryBean<MapperInterface> test_mapper(SqlSessionFactory factory) throws Exception{
 		MapperFactoryBean<MapperInterface> factoryBean = new MapperFactoryBean<MapperInterface>(MapperInterface.class);
 		factoryBean.setSqlSessionFactory(factory);
 		return factoryBean;
--- MiniProject/src/com/demo/mapper/MapperInterface.java
+++ MiniProject/src/com/demo/mapper/MapperInterface.java
@@ -2,9 +2,10 @@ package com.demo.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 
-import beans.DataBean;
+import com.demo.beans.DataBean;
 
 public interface MapperInterface {
+
 	@Insert("insert into spring_mvc_table values(#{data1}, #{data2}, #{data3})")
 	void insert_data(DataBean dataBean);
 }
